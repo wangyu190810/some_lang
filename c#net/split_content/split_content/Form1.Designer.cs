a6586a03834fbcaf4a702bf4b487a4e2551eb452
wangyu <190810401@qq.com>
2017-05-13 21:53:04 +0800

    添加文本分割工具

---
+++ c#net/split_content/split_content/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace split_content
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.新建ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.分割ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.空格分割ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.制表符分割ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.逗号分割ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.分号分割ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.分割ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(432, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem1,
+            this.新建ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.打开ToolStripMenuItem.Text = "文件";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 打开ToolStripMenuItem1
+            // 
+            this.打开ToolStripMenuItem1.Name = "打开ToolStripMenuItem1";
+            this.打开ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.打开ToolStripMenuItem1.Text = "打开";
+            this.打开ToolStripMenuItem1.Click += new System.EventHandler(this.打开ToolStripMenuItem1_Click);
+            // 
+            // 新建ToolStripMenuItem
+            // 
+            this.新建ToolStripMenuItem.Name = "新建ToolStripMenuItem";
+            this.新建ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.新建ToolStripMenuItem.Text = "新建";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // 分割ToolStripMenuItem
+            // 
+            this.分割ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.空格分割ToolStripMenuItem,
+            this.制表符分割ToolStripMenuItem,
+            this.逗号分割ToolStripMenuItem,
+            this.分号分割ToolStripMenuItem});
+            this.分割ToolStripMenuItem.Name = "分割ToolStripMenuItem";
+            this.分割ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.分割ToolStripMenuItem.Text = "分割";
+            // 
+            // 空格分割ToolStripMenuItem
+            // 
+            this.空格分割ToolStripMenuItem.Name = "空格分割ToolStripMenuItem";
+            this.空格分割ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.空格分割ToolStripMenuItem.Text = "空格分割";
+            this.空格分割ToolStripMenuItem.Click += new System.EventHandler(this.空格分割ToolStripMenuItem_Click);
+            // 
+            // 制表符分割ToolStripMenuItem
+            // 
+            this.制表符分割ToolStripMenuItem.Name = "制表符分割ToolStripMenuItem";
+            this.制表符分割ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.制表符分割ToolStripMenuItem.Text = "制表符分割";
+            // 
+            // 逗号分割ToolStripMenuItem
+            // 
+            this.逗号分割ToolStripMenuItem.Name = "逗号分割ToolStripMenuItem";
+            this.逗号分割ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.逗号分割ToolStripMenuItem.Text = "逗号分割";
+            // 
+            // 分号分割ToolStripMenuItem
+            // 
+            this.分号分割ToolStripMenuItem.Name = "分号分割ToolStripMenuItem";
+            this.分号分割ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.分号分割ToolStripMenuItem.Text = "分号分割";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(343, 226);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(349, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "分割";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(349, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "去掉的列";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(349, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(71, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 319);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 新建ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 分割ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 空格分割ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 制表符分割ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 逗号分割ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 分号分割ToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ c#net/split_content/split_content/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace split_content
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+  
+        String filename;
+        bool bIsSave;
+        // 定义列，当前有多少列
+        int col = 0;
+        // 连接字符串
+        char join_str = '\t';
+
+        String split_flag;
+
+        public bool BIsSave
+        {
+            get
+            {
+                return bIsSave;
+            }
+
+            set
+            {
+                bIsSave = value;
+            }
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void 打开ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            {
+                openFileDialog1.FileName = "*.txt";
+                openFileDialog1.Filter = "文本文档(*.txt)|*.txt|所有文件|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(openFileDialog1.FileName, Encoding.Default);
+                    try
+                    {
+                        this.richTextBox2.Text = sr.ReadToEnd();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                   
+                    sr.Close();
+                }
+                filename = openFileDialog1.FileName;
+                this.Text = Path.GetFileName(openFileDialog1.FileName) + " - 记事本";
+                BIsSave = true;
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 空格分割ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String data = "";
+            foreach(String line in this.richTextBox2.Lines)
+            {
+                var val = System.Text.RegularExpressions.Regex.Split(line, @"\s{1,}");
+                data += string.Join("\t", val);
+                data += "\r\n";
+            }
+            this.richTextBox2.Text = data;
+            
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = "d:\\";
+            saveFileDialog1.Filter = "ext files (*.txt)|*.txt|*.xls|All files(*.*)";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK && saveFileDialog1.FileName.Length > 0)
+            {
+                richTextBox2.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                MessageBox.Show("存储文件成功！", "保存文件");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            split_flag = textBox1.Text;
+            String data = "";
+            col = 0;
+            foreach (String line in this.richTextBox2.Lines)
+            {
+                var val = line.Split(char.Parse(split_flag));
+                data += string.Join(this.join_str.ToString(), val);
+                data += "\r\n";
+                if (val.Length > col) {
+                    col = val.Length;
+                }
+            }
+            this.richTextBox2.Text = data;
+            String col_str = "";
+            for (int i=0; i < col; i++)
+            {   if (i == col-1){
+                    col_str += i.ToString();
+                }else{
+                    col_str += i.ToString() + ",";
+                }
+            }
+            this.textBox2.Text = col_str ;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var del_col = textBox2.Text.Split(',');
+            
+            String data = "";
+            
+            foreach (String line in this.richTextBox2.Lines)
+            {
+                String[] val = line.Split(this.join_str);
+                var remove_data = new StringBuilder();
+                foreach(string _line in del_col)
+                { int del_line = -1;
+                    try
+                    {
+                        del_line = int.Parse(_line);
+                    }
+                    catch (Exception  parse_error)
+                    {
+                        Console.WriteLine(parse_error);
+                    }
+                    if (del_line != -1) {
+                        if (del_line < val.Length)
+                        {
+                            for (int i = 0; i < val.Length; i++)
+                            {
+                                if (val[del_line] != val[i])
+                                {
+                                    remove_data.Append(val[i]);
+                                    // 当最后一行时，不再添加分割符号
+                                    if (i != val.Length - 1)
+                                    {
+                                        remove_data.Append(this.split_flag);
+                                    }
+                                    
+                                }
+                            }
+                                
+                        }
+                    }
+                   
+                }
+                //for(int i =0; i < val.Length; i++)
+                //{
+                //    if (i < del_col.Length){
+                //        if (del_col[i] == i.ToString())
+                //        {
+
+                //        }else
+                //        {
+                //            remove_data.Append(del_col[i]);
+                //        }
+                //    }
+                //}
+                //data += string.Join(this.split_flag, val);
+                var new_line = remove_data.ToString();
+             //   var new_line = String.Join(this.split_flag,remove_data);
+                if (new_line != "" && new_line != "\r\n")
+                {
+                    data += new_line;
+                    data += "\r\n";
+                }
+              
+            }
+            this.richTextBox2.Text = data;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
