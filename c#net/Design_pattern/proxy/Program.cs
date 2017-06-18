c2726fc6684fd29e2556ab9277101f2c7ebe35e3
wangyu <190810401@qq.com>
2017-06-18 19:13:11 +0800

    c# mysql

---
+++ c#net/Design_pattern/proxy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+   abstract class Subject{
+    public abstract void Request();   
+   }
+
+   class RealSubject : Subject{
+       public override void Request(){
+           Console.Write("");
+       }
+
+   }
+
+}
---
+++ c#net/Design_pattern/工厂模式/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Design_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            OperateTest Operatetest = new OperateTest();
+            Operatetest.run();
+        }
+    }
+    
+}
