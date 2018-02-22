55795661a4c755e7c08a47f6308c0268a4cd110a
wangyu <190810401@qq.com>
2018-02-22 15:39:34 +0800

    chrome 插件dome

--- clang/computer_system/ExceptionalControlFlow/fork.c
+++ clang/computer_system/ExceptionalControlFlow/fork.c
@@ -2,14 +2,15 @@
 int main()
 {
 
-pid_t pid;
-int x = 1;
+    pid_t pid;
+    int x = 1;
 
-pid = Fork();
-if (pid == 0){
-printf("child: x=%d\n",++x);
-exit(0);
-}
-printf("parent:x=%d\n",--x);
-exit(0);
+    pid = Fork();
+    if (pid == 0)
+    {
+        printf("child: x=%d\n", ++x);
+        exit(0);
+    }
+    printf("parent:x=%d\n", --x);
+    exit(0);
 }
